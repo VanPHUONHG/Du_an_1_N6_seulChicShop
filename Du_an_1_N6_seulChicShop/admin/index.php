58164713446d599b0bb6f5d101b92b80f07d1bfb
Quang Quyen <dangquangquyen1502@gmail.com>
2025-03-14 14:58:48 +0700

    update dashboard admin

--- Du_an_1_N6_seulChicShop/admin/index.php
+++ Du_an_1_N6_seulChicShop/admin/index.php
@@ -5,12 +5,14 @@ require_once '../commons/env.php'; // Khai báo biến môi trường
 require_once '../commons/function.php'; // Hàm hỗ trợ
 
 // Require toàn bộ file Controllers
+require_once './controllers/AdminDashboardController.php';
 require_once './controllers/AdminDanhMucController.php';
 require_once './controllers/AdminSanPhamController.php';
-
 // Require toàn bộ file Models
 require_once './models/AdminDanhMuc.php';
 require_once './models/AdminSanPham.php';
+require_once './models/AdminOrder.php';
+require_once './models/AdminUser.php';
 
 
 // Route
@@ -19,12 +21,11 @@ $act = $_GET['act'] ?? '/';
 // Để bảo bảo tính chất chỉ gọi 1 hàm Controller để xử lý request thì mình sử dụng match
 
 match ($act) {
-
+  '/' => (new AdminDashboardController())->trangChu(),
   //router danh muc
   'danh-muc' => (new AdminDanhMucController())->danhSachDanhMuc(),
   'form-them-danh-muc' => (new AdminDanhMucController())->formAddDanhMuc(),
   'them-danh-muc' => (new AdminDanhMucController())->possAddDanhMuc(),
   'form-sua-danh-muc' => (new AdminDanhMucController())->formEditDanhMuc(),
   'sua-danh-muc' => (new AdminDanhMucController())->possEditDanhMuc(),
- 
 };
