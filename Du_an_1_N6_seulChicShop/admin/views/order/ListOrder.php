f3e172997a96027a5f1cc3e97b79eee529695f74
Quang Quyen <dangquangquyen1502@gmail.com>
2025-03-16 14:28:47 +0700

    Edit again branch

---
+++ Du_an_1_N6_seulChicShop/admin/views/order/ListOrder.php
@@ -0,0 +1,84 @@
+<?php include './views/layout/header.php'; ?>
+<?php include './views/layout/navbar.php'; ?>
+<?php include './views/layout/sidebar.php'; ?>
+
+<div class="content-wrapper">
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Quản lý đơn hàng</h1>
+                </div>
+            </div>
+    </section>
+
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+
+                        <div class="card-body">
+                            <table id="example1" class="table table-bordered table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>STT</th>
+                                        <th>Mã đơn hàng</th>
+                                        <th>Tên người nhận</th>
+                                        <th>Số điện thoại</th>
+                                        <th>Ngày đặt</th>
+                                        <th>Tổng tiền</th>
+                                        <th>Trạng thái</th>
+                                        <th>Thao tác</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php foreach ($listOrder as $key => $order): ?>
+
+                                        <tr>
+                                            <td><?= $key + 1 ?></td>
+                                            <td><?= $order['ma_don_hang'] ?></td>
+                                            <td><?= $order['ten_nguoi_nhan'] ?></td>
+                                            <td><?= $order['sdt_nguoi_nhan'] ?></td>
+                                            <td><?= $order['ngay_dat'] ?></td>
+                                            <td><?= $order['tong_tien'] ?></td>
+                                            <td><?= $order['ten_trang_thai'] ?></td>
+                                            <td>
+                                                <a href="<?= BASE_URL_ADMIN . '?act=form-sua-don-hang&id_don_hang=' . $order['id'] ?>"><button class="btn btn-warning">Chi tiết Đơn Hàng </button></a>
+                                            </td>
+                                        </tr>
+                                    <?php endforeach ?>
+                                </tbody>
+
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+<?php include './views/layout/footer.php'; ?>
+
+<script>
+    $(function() {
+        $("#example1").DataTable({
+            "responsive": true,
+            "lengthChange": false,
+            "autoWidth": false,
+            // "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+        }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+        $('#example2').DataTable({
+            "paging": true,
+            "lengthChange": false,
+            "searching": false,
+            "ordering": true,
+            "info": true,
+            "autoWidth": false,
+            "responsive": true,
+        });
+    });
+</script>
+</body>
+
+</html>
